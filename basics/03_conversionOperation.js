eb8f2dc5675b63798243fff81b85aa0ec3ec7439
tarunrajput05 <165491474+tarunrajput05@users.noreply.github.com>
2026-02-21 09:52:11 +0000

    done

---
+++ basics/03_conversionOperation.js
@@ -0,0 +1,16 @@
+let score ="31"
+//console.log(typeof 33);
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+console.log(valueInNumber);
+
+let isLoggedIn = ""
+let booleanIsloggedIn=Boolean(isLoggedIn)
+console.log(booleanIsloggedIn);
+
+let someNumber="76"
+let stringNumber=String(someNumber)
+console.log(typeof stringNumber);
+
+console.log(stringNumber);
