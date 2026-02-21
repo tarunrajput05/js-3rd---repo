68843f9186e4863c7c3c45727bc2560532607065
tarunrajput05 <165491474+tarunrajput05@users.noreply.github.com>
2026-02-21 09:17:50 +0000

    done

--- basics/dataType.js
+++ basics/dataType.js
@@ -4,3 +4,19 @@
 console.log(3+3); 
 console.log("tarun");
 
+console.log(typeof "tarun");
+let name="tarun"
+let age=21
+let isLoggedIn=false
+console.table([typeof false,typeof "tarun",typeof age,typeof undefined, typeof null]);
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+
