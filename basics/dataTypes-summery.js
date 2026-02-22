a893d657d09595a750c9feb4649d559f827f4e76
tarunrajput05 <165491474+tarunrajput05@users.noreply.github.com>
2026-02-22 08:42:54 +0000

    done

---
+++ basics/dataTypes-summery.js
@@ -0,0 +1,48 @@
+// primitive
+
+// 7 type= call  by value
+//strin, number , boolean,null ,undefined,symbol(uniquie value bananen ke liye use hota hai),BigInt
+
+
+// javascripts static or dynamic
+// not language define in javascript
+// Reference (non primitive)
+//  Primitive
+
+//  7 types : String, Number, Boolearn, null, undefined, Symbol, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+// const bigNumber = 3456543576654356754n
+
+
+
+// Reference (Non primitive)
+
+// Array, Objects, Functions
+
+const heros = ["shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello world");
+}
+
+console.log(typeof anotherId);
+
+// https://262.ecma-international.org/5.1/#sec-11.4.3
+
+//Array ,objects, Functions
